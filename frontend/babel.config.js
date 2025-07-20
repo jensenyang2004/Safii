248e6cbe46e0179f164851419f3b7acd4042a5ee
Koikbin <kaoming777@gmail.com>
2025-07-20 16:35:36 +0800

    WIP: update babel & iOS pods

--- frontend/babel.config.js
+++ frontend/babel.config.js
@@ -1,9 +1,21 @@
-module.exports = function (api) {
-  api.cache(true);
-  return {
-    presets: [
-      ["babel-preset-expo", { jsxImportSource: "nativewind" }],
-      "nativewind/babel",
-    ],
-  };
-};
+
+// module.exports = function (api) {
+//   api.cache(true);
+//   return {
+//     presets: [
+//       // ["babel-preset-expo", { jsxImportSource: "nativewind" }],
+//       'babel-preset-expo',
+    
+//     ],
+//     plugins: [
+//       // NativeWind (Tailwind-in-JSX) support
+//       'nativewind/babel',
+
+//       // Expo Router – transforms your file-based routes
+//       'expo-router/babel',
+
+//       // React-Native-Reanimated – MUST be last
+//       'react-native-reanimated/plugin',
+//     ],
+//   };
+// };
