adc56f0536c028d08b3e6c6b5f44ba743dc711ee
Jing-Cheng <jenseniu0810@gmail.com>
2025-09-22 16:33:18 +0800

    feat: update to SDK 54

--- frontend/eslint.config.js
+++ frontend/eslint.config.js
@@ -6,5 +6,18 @@ module.exports = defineConfig([
   expoConfig,
   {
     ignores: ["dist/*"],
-  }
+  },
+  {
+    settings: {
+      "import/resolver": {
+        typescript: {
+          alwaysTryTypes: true,
+          project: ["./tsconfig.json"],
+        },
+        node: {
+          extensions: [".js", ".jsx", ".ts", ".tsx", ".d.ts"],
+        },
+      },
+    },
+  },
 ]);
