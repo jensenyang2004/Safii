c5ab16b948dcda7014b1577faf46c8b947b29268
Koikbin <kaoming777@gmail.com>
2025-11-01 17:04:46 +0100

    map func with police data

--- frontend/context/NotificationProvider.tsx
+++ frontend/context/NotificationProvider.tsx
@@ -2,7 +2,7 @@ import React, { createContext, useContext, useEffect, useState } from 'react';
 import { Alert, Button, View } from 'react-native';
 import { collection, query, where, onSnapshot, getDoc, doc as docRef, doc } from 'firebase/firestore';
 import { db } from '@/apis/firebase';
-import { registerForPushNotificationsAsync, saveTokenToFirestore, sendPushNotification } from '@/libs/notifications';
+import { registerForPushNotificationsAsync, saveTokenToFirestore, sendPushNotification } from '@/apis/notifications';
 import { useAuth } from '@/context/AuthProvider'; // Import AuthProvider context
 import { useNavigation } from '@react-navigation/native';
 import { StackNavigationProp } from '@react-navigation/stack';
